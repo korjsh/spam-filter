ca5174b48d6a3c97b2423b14fa51640bfd9d8e5f
korjsh <korjsh91@gmail.com>
2022-08-06 12:51:33 +0900

    specification case

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export function isSpam(content: string, spamLinkDomains: string[], redirectionDepth: number ): boolean {
+  let result: boolean = false
+  const requestUrl = content.split(' ')[2]
+  const redirectUrl = addressRedirect(requestUrl, redirectionDepth)
+
+  for (const domain of spamLinkDomains) {
+    if(redirectUrl.includes(domain))
+    {
+      result = true;
+      break;
+    } 
+  }
+  return result
+}
+
+
+function addressRedirect(link: string, depth: number) {
+  if (link === 'https://moimstg.page.link/dmCn') {
+    const redirectList: Array<string> = ['https://moimstg.page.link/qbvQ', 'https://github.com', 'https://lab.github.com']
+    const redirectResultUrl: string = redirectList[depth-1]
+    return redirectResultUrl
+  } else {
+    throw new Error('not permitted url')
+  }
+}
+console.log(isSpam("spam spam https://moimstg.page.link/dmCn", ["lab.github.com"], 1))
+console.log(isSpam("spam spam https://moimstg.page.link/dmCn", ["moimstg.page.link"], 1))
+console.log(isSpam("spam spam https://moimstg.page.link/dmCn", ["github.com"], 2))
+console.log(isSpam("spam spam https://moimstg.page.link/dmCn", ["lab.github.com"], 2))
+console.log(isSpam("spam spam https://moimstg.page.link/dmCn", ["lab.github.com"], 3))
+console.log(isSpam("spam spam https://moimstg.page.link/dmCn", ["lab.github.com"], 2))
---
+++ src/spam.test.js
@@ -0,0 +1,2 @@
+
+expect(isSpam("spam spam https://moimstg.page.link/dmCn", ["lab.github.com"], 1)).to.eq(false)
